3531f712cb9bffb7a492ed3e9d32bbc84f10a5a2
Anushree-SrivatsaYeri <91054203+Anushree-SrivatsaYeri@users.noreply.github.com>
2021-10-25 20:29:09 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -2,12 +2,17 @@
 #define STATS_H 
 #include <math.h>
 
+#ifdef NAN
+/* NAN is supported */
+#endif
+
 struct Stats 
 {
-	float average;
+    float average;
     float min ;
     float max;
 };
 
 extern  Stats compute_statistics(const float* numberset, int setlength);
-
+Stats compute_statistics(const int numberset, int setlength) ;
+#endif
